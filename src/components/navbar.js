b92423b9aec8db95c801995d98371b15e8dac784
Jason <jasonliang2002@gmail.com>
2024-04-12 19:08:00 -0700

    Init commit files

---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import elmologo from '../assets/elmologo.svg'
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <navbar className="navbar">
+        <div className='navbar-container'>
+            <div className='elmo-logo'>
+                <a href='#home'>
+                    <img className='elmo-icon' src={elmologo} alt='Logo'/>
+                </a>
+            </div>
+            <div className='nav-elements'>
+                <ul id='menu'>
+                    <li className='menu-item'><a href='#about'> ABOUT </a></li>
+                    <li className='menu-item'><a href='#cv'> CV </a></li>
+                    <li className='menu-item'><a href='#hobbies'>HOBBIES</a></li>
+                    <li className='contact-button'><a href='#contact'>CONTACT</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </navbar>
+  )
+}
+
+export default Navbar
